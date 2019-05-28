5af5c62d87be293d17dc7289e3ee6931a0a4baa4
ha271923 <ha271923@gmail.com>
2019-05-28 19:04:18 +0800

    Added cspirat answer

---
+++ app/src/main/java/com/cspirat/TreeNode.java
@@ -0,0 +1,16 @@
+package com.cspirat;
+
+/**
+ * Created by Edward on 23/07/2017.
+ */
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode (int x) {
+        val = x;
+    }
+
+}
