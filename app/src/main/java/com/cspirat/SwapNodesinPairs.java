79f763a1c9ffcc3063528708ee7fb97d28e760fd
ha271923 <ha271923@gmail.com>
2019-07-18 01:08:51 +0800

    FindFirstandLastPositionofElementinSortedArray

--- app/src/main/java/com/cspirat/SwapNodesinPairs.java
+++ app/src/main/java/com/cspirat/SwapNodesinPairs.java
@@ -42,4 +42,30 @@ public class SwapNodesinPairs {
         }
         return dummyhead.next;
     }
+
+    // https://leetcode.wang/leetCode-24-Swap-Nodes-in-Pairs.html
+    public ListNode swapPairs2(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode point = dummy;
+        while (point.next != null && point.next.next != null) {
+            ListNode swap1 = point.next;
+            ListNode swap2 = point.next.next;
+            point.next = swap2;
+            swap1.next = swap2.next;
+            swap2.next = swap1;
+            point = swap1;
+        }
+        return dummy.next;
+    }
+
+    // https://leetcode.wang/leetCode-24-Swap-Nodes-in-Pairs.html
+    public ListNode swapPairs_recursive(ListNode head) {
+        if ((head == null)||(head.next == null))
+            return head;
+        ListNode n = head.next;
+        head.next = swapPairs(head.next.next);
+        n.next = head;
+        return n;
+    }
 }
