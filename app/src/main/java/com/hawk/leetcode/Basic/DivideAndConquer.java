47641939251b70e564683b0112b2cd9ecba67e45
ha271923 <ha271923@gmail.com>
2019-08-08 11:50:25 +0800

    DivideAndConquer EMPTY

--- app/src/main/java/com/hawk/leetcode/Basic/DivideAndConquer.java
+++ app/src/main/java/com/hawk/leetcode/Basic/DivideAndConquer.java
@@ -1,7 +1,20 @@
 package com.hawk.leetcode.Basic;
 /**
+ * 3. 分治(divide-and-conquer)演算法:
+ * 若是問題 規模(problem size)夠小， 就直接解決之；反之，若問題規模較大，就將其分割(divide)成兩
+ * 個或多個子問題(分割階段)，然後將每個子問題遞迴地(recursively)征 服(conquer)解決之(征服階段)
+ * 。之後再將每個子 問題的解答合併(merge)，使其成為原問題的解答(合併階段)。
+ *   3.1 merge sort
+ *   3.2 quick sort
+ *   3.3 分治缺陷棋盤填滿(tiling a defective chessboard)演算法
+ *   3.4 分治二維求 秩(2D rank finding)演算法
+ *   3.5 分治二維最大點(2D maxima finding)演算法
+ *   3.6 分治最近二 維點對(closest pair of 2D points)演算法
+ *
  * Divide-and-conquer
  *  ex:Merge k sorted lists
+ *  QuickSort
+ *  MergeSort
  *
  * Problems:
  * https://www.zybuluo.com/Yano/note/253633
