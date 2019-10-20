33def7da6a989f5128f4e29227578725e9b10528
ha271923 <ha271923@gmail.com>
2019-10-20 15:45:32 +0800

    update

--- app/src/main/java/com/cspirat/ValidSudoku.java
+++ app/src/main/java/com/cspirat/ValidSudoku.java
@@ -10,52 +10,119 @@ import java.util.HashSet;
  * Date : Sep, 2017
  * Description : 36. Valid Sudoku
  */
+// QUESTION: 這題只是求'驗證'出這樣的題目是否違反sudoku的規則,
+//           並非解出此題, 也非要求一定藥可以解出來
+// KEY: HashSet
 public class ValidSudoku {
 
+    public static void main(String[] args) {
+        char[][] board1 = {
+                // Ans: true
+                {'5','3','.',  '.','7','.',  '.','.','.'},
+                {'6','.','.',  '1','9','5',  '.','.','.'},
+                {'.','9','8',  '.','.','.',  '.','6','.'},
+
+                {'8','.','.',  '.','6','.',  '.','.','3'},
+                {'4','.','.',  '8','.','3',  '.','.','1'},
+                {'7','.','.',  '.','2','.',  '.','.','6'},
+
+                {'.','6','.',  '.','.','.',  '2','8','.'},
+                {'.','.','.',  '4','1','9',  '.','.','5'},
+                {'.','.','.',  '.','8','.',  '.','7','9'}
+        };
+
+        char[][] board2 = {
+                // Ans: false
+                {'8','3','.',  '.','7','.',  '.','.','.'},
+                {'6','.','.',  '1','9','5',  '.','.','.'},
+                {'.','9','8',  '.','.','.',  '.','6','.'},
+
+                {'8','.','.',  '.','6','.',  '.','.','3'},
+                {'4','.','.',  '8','.','3',  '.','.','1'},
+                {'7','.','.',  '.','2','.',  '.','.','6'},
+
+                {'.','6','.',  '.','.','.',  '2','8','.'},
+                {'.','.','.',  '4','1','9',  '.','.','5'},
+                {'.','.','.',  '.','8','.',  '.','7','9'}
+        };
+
+        boolean ret = isValidSudoku_HashSet(board2);
+        // boolean ret = isValidSudoku_BruteForce(board1);
+        System.out.println("Valided ret=" + ret);
+    }
 
     // time : O(n^2) space : O(n)
-    public boolean isValidSudoku1(char[][] board) {
-        for (int i = 0; i < board.length; i++) {
+    public static boolean isValidSudoku_HashSet(char[][] board) {
+        char curDigital;
+        // board.length == board[0].length == 9
+        // Tips: 2個LOOP = 9x9= 81次填值
+        for (int i = 0; i < board[0].length; i++) { // xy1=i LOOP1: 9: Try to fill num 1~9 to field by len = 1~9
+            // KEY: 利用Set資料結構
+            // KEY: add(E)的返回值, ret = true if this set did not already contain the specified element
             HashSet<Character> rows = new HashSet<>();
             HashSet<Character> cols = new HashSet<>();
             HashSet<Character> cube = new HashSet<>();
-            for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] != '.' && !rows.add(board[i][j])) return false;
-                if (board[j][i] != '.' && !cols.add(board[j][i])) return false;
 
+            for (int j = 0; j < board[0].length; j++) { // xy2=j LOOP2: 9: Try to fill num 1~9 to field by len = 1~9
+                // x: 掃行
+                curDigital = board[j][i];
+                if (curDigital != '.' && !cols.add(curDigital)) // COL: 已知數 且 此數值不存在於Set
+                    return false;
+
+                // y: 掃列
+                curDigital = board[i][j];
+                if (curDigital != '.' && !rows.add(curDigital)) // ROW: 已知數 且 此數值不存在於Set
+                    return false;
+
+                // 掃9x9宮
                 int rowIndex = 3 * (i / 3);
                 int colIndex = 3 * (i % 3);
-
-                if (board[rowIndex + j / 3][colIndex + j % 3] != '.' && !cube.add(board[rowIndex + j / 3][colIndex + j % 3]))
+                curDigital = board[rowIndex + j / 3][colIndex + j % 3];
+                if (curDigital != '.' && !cube.add(curDigital)) // 9x9: 已知數 且 此數值不存在於Set
                     return false;
             }
         }
         return true;
     }
 
-    public boolean isValidSudoku2(char[][] board) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] == '.') continue;
-                if (!valid(board, i, j)) return false;
+    // No HashSet
+    // Tips: 9x9 x (9x9x9) = 81次填值 x 729次填後比對 = 共 59049 次Loop
+    public static boolean isValidSudoku_BruteForce(char[][] board) {
+        for (int y = 0; y < board.length; y++) {
+            for (int x = 0; x < board[0].length; x++) {
+                if (board[y][x] == '.') // 掃到未知數
+                    continue;
+                if (!valid(board, y, x)) // 一定是已知數
+                    return false;
             }
         }
         return true;
     }
+    // 驗證
+    public static boolean valid(char[][] board, int y, int x) {
+        char curDigital = board[y][x];
 
-    public boolean valid(char[][] board, int i, int j) {
-        for (int row = 0; row < board.length; row++) {
-            if (row == i) continue;
-            if (board[row][j] == board[i][j]) return false;
-        }
+        // x: 掃行
         for (int col = 0; col < board[0].length; col++) {
-            if (col == i) continue;
-            if (board[i][col] == board[i][j]) return false;
+            if (col == x)
+                continue;
+            if (board[y][col] == curDigital)
+                return false;
         }
-        for (int row = (i / 3) * 3; row < (i / 3 + 1) * 3; row++) {
-            for (int col = (j / 3) * 3; col < (j / 3 + 1) * 3; col++) {
-                if (row == i && col == j) continue;
-                if (board[row][col] == board[i][j]) return false;
+        // y: 掃列
+        for (int row = 0; row < board[0].length; row++) {
+            if (row == y)
+                continue;
+            if (board[row][x] == curDigital)
+                return false;
+        }
+        // 掃9x9宮
+        for (int row = (y / 3) * 3; row < (y / 3 + 1) * 3; row++) {
+            for (int col = (x / 3) * 3; col < (x / 3 + 1) * 3; col++) {
+                if (row == y && col == x)
+                    continue;
+                if (board[row][col] == curDigital)
+                    return false;
             }
         }
         return true;
