d30c43b8c7f8e93a734a2721bfa63f094c710b20
ha271923 <ha271923@gmail.com>
2019-11-25 15:53:23 +0800

    update

--- app/src/main/java/com/cspirat/Permutations.java
+++ app/src/main/java/com/cspirat/Permutations.java
@@ -1,7 +1,10 @@
 package com.cspirat;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public class Permutations {
     /**
@@ -34,35 +37,94 @@ public class Permutations {
      * @param nums
      * @return
      */
-    // time : O(n! * n) space : O(n);
-    public static List<List<Integer>> permute(int[] nums) {
+    // https://github.com/billryan/algorithm-exercise/blob/master/zh-hans/exhaustive_search/permutations.md
+    // https://blog.csdn.net/tuantuanls/article/details/8717262
+    public static void main(String[] args) {
+        int[] input = {1,2,3}; // Ans: 6 種排列組合
+        List<List<Integer>> ret;
+        // ret = permute_list(input);
+        ret = permute_stack(input);
+        // ret = permute_swap(input);
+        // ret = permute_recursive(input);
+        System.out.println("ANS: ret.size()="+ret.size());
+    }
+
+/*
+ A. 重新填值 = permute_list()
+
+                  /--add(2)--> [1,2] ---add(3)--> [1,2,3]
+                 /
+             /[1]----add(3)--> [1,3] ---add(2)--> [1,3,2]
+            /
+           /      /--add(1)--> [2,1] ---add(3)--> [2,1,3]
+  {1,2,3}  ---[2]
+           \      \--add(3)--> [2,3] ---add(1)--> [2,3,1]
+            \
+             \[3]----add(1)--> [3,1] ---add(2)--> [3,1,2]
+                 \
+                  \--add(2)--> [3,2] ---add(1)--> [3,2,1]
+*/
+    // time : O(n! * n)  ,  space : O(n);
+    public static List<List<Integer>> permute_list(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        if (nums == null || nums.length == 0) return res;
-        helper(res, new ArrayList<>(), nums);
+        if (nums == null || nums.length == 0)
+            return res;
+        dfs(res, new ArrayList<>(), nums);
         return res;
     }
 
-    public static void helper(List<List<Integer>> res, List<Integer> list, int[] nums) {
+    //    void add (int index, E element)
+    // boolean add (E e) <------------ call it to add value at end
+    //       E remove (int index) <--- call it to remove one of element
+    // boolean remove (Object o)
+    public static void dfs(List<List<Integer>> res, List<Integer> list, int[] nums) {
+        System.out.println(list);
         if (list.size() == nums.length) {
             res.add(new ArrayList<>(list));
-            return;
+            return; // return-1: 一旦字數量達標時 ++
         }
+        // Process
         for (int i = 0; i < nums.length; i++) {
-            if (list.contains(nums[i])) continue;  // O(n)
-            list.add(nums[i]);
-            helper(res, list, nums);
-            list.remove(list.size() - 1);
+            // 以上是上半場開始 {
+            if (list.contains(nums[i])) // KEY: 列舉不同數
+                continue;  // O(n)
+            list.add(nums[i]);               // ++ add(value)
+            // 以上是上半場結束 }
+            dfs(res, list, nums);            // recursive
+            // 以下是下半場開始 {
+            list.remove(list.size() - 1); // -- remove(LastE)
+            // 以下是下半場結束 }
         }
+        // return-2: 一旦字數量達標時 --
     }
 
-    // time : O(n!) space : O(n);
-    public static List<List<Integer>> permute2(int[] nums) {
+/*
+ B: 倆倆互換 = permute_swap()
+   PS: sw(l,r) == swap l and r value
+
+                          /- sw(2,2) -> [1,2,3]
+                         /
+             /----[1,2,3]--- sw(2,3) -> [1,3,2]
+         sw(1,1)
+        /                 /- sw(2,2) -> [2,1,3]
+ [1,2,3]-sw(1,2)--[2,1,3]
+        \                 \- sw(2,3) -> [2,3,1]
+         sw(1,3)
+             \----[3,2,1]--- sw(2,2) -> [3,1,2]
+                         \
+                          \- sw(2,3) -> [3,2,1]
+
+ */
+    // time : O(n!)  ,   space : O(n);
+    public static List<List<Integer>> permute_swap(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        if (nums == null || nums.length == 0) return res;
-        helper2(res, 0, nums);
+        if (nums == null || nums.length == 0)
+            return res;
+        dfs_offset(res, 0, nums);
         return res;
     }
-    public static void helper2(List<List<Integer>> res, int start, int[] nums) {
+    public static void dfs_offset(List<List<Integer>> res, int start, int[] nums) {
+        System.out.println(res);
         if (start == nums.length) {
             List<Integer> list = new ArrayList<>();
             for (int num : nums) {
@@ -73,13 +135,84 @@ public class Permutations {
         }
         for (int i = start; i < nums.length; i++) {
             swap(nums, start, i);
-            helper2(res, start + 1, nums);
+            dfs_offset(res, start + 1, nums);
             swap(nums, start, i);
         }
     }
     public static void swap(int[] nums, int l, int r) {
+        System.out.println("+++ nums+"+Arrays.toString(nums)+" Swap("+l+","+r+")");
         int temp = nums[l];
         nums[l] = nums[r];
         nums[r] = temp;
+        System.out.println("--- nums+"+ Arrays.toString(nums)+" Swap("+l+","+r+")");
     }
+
+
+/*
+    C: 遞歸 = recursive
+        與題解1基於子集的模板不同，這裡我們直接從全排列的數學定義初始出發，要
+        求給定數組的全分開，可將其模擬為某個袋子裡有編號為1到n的球，將其放入n
+        個不同的盒子怎麼放？基本思路就是從袋子裡逐個拿球放入盒子，直到袋子裡的
+        球拿完為止，拿完時即為一種放法。
+*/
+    static public List<List<Integer>> permute_recursive(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        List<Integer> numsList = new ArrayList<>();
+
+        if (nums == null) {
+            return res;
+        } else {
+            // convert int[] to List<Integer>
+            for (int item : nums)
+                numsList.add(item);
+        }
+
+        if (nums.length <= 1) {
+            res.add(numsList);
+            return res;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int[] numsNew = new int[nums.length - 1];
+            System.arraycopy(nums, 0, numsNew, 0, i);
+            System.arraycopy(nums, i + 1, numsNew, i, nums.length - i - 1);
+
+            List<List<Integer>> resTemp = permute_recursive(numsNew);
+            for (List<Integer> temp : resTemp) {
+                temp.add(nums[i]);
+                res.add(temp);
+            }
+        }
+        return res;
+    }
+
+
+/*
+
+  A-2: 重新填值 = stack
+
+ */
+    static public List<List<Integer>> permute_stack(int[] nums) {
+        List<List<Integer>> result = new LinkedList<>();
+        dfs2(result, new Stack<Integer>(), nums);
+        return result;
+    }
+
+    static private void dfs2(List<List<Integer>> res, Stack<Integer> list, int[] nums) {
+        if (list.size() == nums.length) {
+            res.add(new ArrayList<>(list));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (list.contains(nums[i])) {
+                continue;
+            }
+            list.push(nums[i]);
+            dfs2(res, list, nums);
+            list.pop();
+        }
+    }
+
+
 }
--- app/src/main/java/com/cspirat/PermutationsII.java
+++ app/src/main/java/com/cspirat/PermutationsII.java
@@ -2,7 +2,9 @@ package com.cspirat;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Project Name : Leetcode
@@ -17,6 +19,7 @@ public class PermutationsII {
      * 47. Permutations II
      * For example,
      [1,1,2] have the following unique permutations:
+
      [
      [1,1,2],
      [1,2,1],
@@ -27,38 +30,56 @@ public class PermutationsII {
      * @param nums
      * @return
      */
+    // Q: 相較於Permutations, 列舉的元素可能是重複元素
+    public static void main(String[] args) {
+        List<List<Integer>> res = permute_list(new int[]{1, 1, 2});
+        // List<List<Integer>> res = permute_swap(new int[]{1, 1, 2});
+        for (List<Integer> l : res)
+            System.out.println(l);
+    }
+
     // time : O(n!) space : O(n);
-    public List<List<Integer>> permuteUnique(int[] nums) {
+    static public List<List<Integer>> permute_list(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        if (nums == null || nums.length == 0) return  res;
-        Arrays.sort(nums);
-        helper(res, new ArrayList<>(), nums, new boolean[nums.length]);
+        if (nums == null || nums.length == 0)
+            return  res;
+        Arrays.sort(nums); // KEY: 排序是為了後面nums[i] == nums[i - 1]的algorithm算法
+        dfs(res, new ArrayList<>(), nums, new boolean[nums.length]); // boolean[]  作為列舉紀錄資訊
         return res;
     }
 
-    public void helper(List<List<Integer>> res, List<Integer> list, int[] nums, boolean[] used) {
+    static public void dfs(List<List<Integer>> res, List<Integer> list, int[] nums, boolean[] used) {
         if (list.size() == nums.length) {
             res.add(new ArrayList<>(list));
         }
         for (int i = 0; i < nums.length; i++) {
-            if (used[i] || i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
-            used[i] = true;
+            if (  used[i] )   // KEY: 元素已列舉過
+                continue;
+            if ( i > 0 && !used[i - 1] && nums[i] == nums[i - 1]) // KEY: 難理解??? 試著把此判斷式拿掉以了解用途
+            {
+                System.out.println("  nums["+i+"]="+nums[i]+ " nums["+(i-1)+"]="+nums[i - 1]);
+                continue;
+            }
             list.add(nums[i]);
-            helper(res, list, nums, used);
-            used[i] = false;
+            used[i] = true;  // Tips++: 代表nums[i]此元素已列舉過used[i]=true
+            dfs(res, list, nums, used);
+            used[i] = false; // Tips--: 代表nums[i]消除此元素列舉used[i]=false
             list.remove(list.size() - 1);
         }
     }
 
+    // ----------------------------------------------------------------------------------
+
     // time : O(n!) space : O(n);
-    public List<List<Integer>> permuteUnique2(int[] nums) {
+    static public List<List<Integer>> permute_swap(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        if (nums == null || nums.length == 0) return  res;
-        Arrays.sort(nums);
-        helper2(res, nums, 0);
+        if (nums == null || nums.length == 0)
+            return  res;
+        Arrays.sort(nums); // KEY: 排序是為了後面
+        dfs_offset(res, nums, 0);
         return res;
     }
-    public void helper2(List<List<Integer>> res, int[] nums, int start) {
+    static public void dfs_offset(List<List<Integer>> res, int[] nums, int start) {
         if (start == nums.length) {
             List<Integer> list = new ArrayList<>();
             for (int num : nums) {
@@ -69,22 +90,24 @@ public class PermutationsII {
         }
 
         for (int i = start; i < nums.length; i++) {
-            if (isUsed(nums,start, i)) continue;
+            if (isUsed(nums,start, i))
+                continue;
             swap(nums, start, i);
-            helper2(res, nums, start + 1);
+            dfs_offset(res, nums, start + 1);
             swap(nums, start, i);
         }
     }
 
-    public void swap(int[] nums, int i, int j) {
+    static public void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
 
-    public boolean isUsed(int[] nums, int i, int j) {
+    static public boolean isUsed(int[] nums, int i, int j) {
         for (int x = i; x < j; x++) {
-            if (nums[x] == nums[j]) return true;
+            if (nums[x] == nums[j])
+                return true;
         }
         return false;
     }
