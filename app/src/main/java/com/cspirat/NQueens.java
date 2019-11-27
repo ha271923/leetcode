77d0b0797f7ba9955a44019a2ff8065147a65ca1
ha271923 <ha271923@gmail.com>
2019-11-28 01:15:47 +0800

    update

--- app/src/main/java/com/cspirat/NQueens.java
+++ app/src/main/java/com/cspirat/NQueens.java
@@ -13,55 +13,131 @@ import java.util.List;
  */
 public class NQueens {
     /**
-     * [
-     [".Q..",  // Solution 1
-     "...Q",
-     "Q...",
-     "..Q."],
-
-     ["..Q.",  // Solution 2
-     "Q...",
-     "...Q",
-     ".Q.."]
+     PASS= 2 solutions
+     [
+         [
+             ".Q..",  // Solution 1
+             "...Q",
+             "Q...",
+             "..Q."
+         ]
+         ,
+         [
+             "..Q.",  // Solution 2
+             "Q...",
+             "...Q",
+             ".Q.."
+         ]
      ]
+
+     FAILED= 5 cases
+     [
+         [
+             "Q...",
+             "..Q.",
+             "XXXX", // FAILED 1
+         ],
+         [
+             "Q...",
+             "...Q",
+             "XXXX", // FAILED 2
+         ],
+         [
+             "...Q",
+             "Q...",
+             "..Q.",
+             ".X.." // FAILED 3
+         ],
+         [
+             "...Q",
+             ".Q..",
+             "XXXX", // FAILED 4
+         ]
+         [
+             "...Q",
+             "Q...",
+             "..Q.",
+             "XXXX" // FAILED 5
+         ]
+     ]
+
      * time : O(n^n) 不是很确定
      * space : O(n)
 
      * @param n
      * @return
      */
+    public static void main(String[] args) {
+        List<List<String>> ret;
+        ret = solveNQueens(4);
+        printAnswer(ret);
+    }
+    static private void printAnswer(List<List<String>> result){
+        for(int i = 0; i< result.size(); i++) {
+            for(int j=0; j<result.get(i).size(); j++){
+                System.out.println(result.get(i).get(j)+",");
+            }
+            System.out.println("");
+        }
+    }
 
-    public List<List<String>> solveNQueens(int n) {
+    /*
+        if n=4,
+        Layer0-Loop , x= 0 -> check_8ways() = OK -> PUSH Recursive
+            Layer1-Loop , x= 0 -> check_8ways() = Fail
+            Layer1-Loop , x= 1 -> check_8ways() = Fail
+            Layer1-Loop , x= 2 -> check_8ways() = OK -> PUSH by Recursive
+                Layer2-Loop , x= 0 -> check_8ways() = Fail
+                Layer2-Loop , x= 1 -> check_8ways() = Fail
+                Layer2-Loop , x= 2 -> check_8ways() = Fail
+                Layer2-Loop , x= 3 -> check_8ways() = OK -> PUSH by Recursive
+                    Layer3-Loop , x= 0 -> check_8ways() = Fail
+                    Layer3-Loop , x= 1 -> check_8ways() = Fail
+                    Layer3-Loop , x= 2 -> check_8ways() = Fail
+                    Layer3-Loop , x= 3 -> check_8ways() = Fail -> POP by return
+        ...
+
+     */
+    static public List<List<String>> solveNQueens(int n) {
         List<List<String>> res = new ArrayList<>();
-        if (n <= 0) return res;
-        helper(res, new int[n], 0);
+        if (n <= 0)
+            return res;
+        force_scan_recursive(res, new int[n], 0);
         return res;
     }
-    public void helper(List<List<String>> res, int[] queens, int pos) {
-        if (pos == queens.length) {
+    static public void force_scan_recursive(List<List<String>> res, int[] queens, int pos) {
+        if (pos == queens.length) { // 注意: pos=0~4, not 0~3
             addSolution(res, queens);
             return;
         }
-        for (int i = 0; i < queens.length; i++) {
-            queens[pos] = i;
-            if (isValid(queens, pos)) {
-                helper(res, queens, pos + 1);
+        for (int x = 0; x < queens.length; x++) { // 該列每一位置
+            queens[pos] = x;
+            if (check_8Ways(queens, pos)) {
+                force_scan_recursive(res, queens, pos + 1);
             }
         }
     }
 
-    public boolean isValid(int[] queens, int pos) {
-        for (int i = 0; i < pos; i++) {
-            if (queens[i] == queens[pos]) { // 同一列
+    static public boolean check_8Ways(int[] queens, int next_row_x) {
+        for (int x = 0; x < next_row_x; x++) {
+            if (queens[x] == queens[next_row_x]) { // 同一行
                 return false;
-            } else if (Math.abs(queens[pos] - queens[i]) == Math.abs(i - pos)) { //是否在对角线上
+            }
+            // 以下檢查x是否在对角线上發生碰撞? ex: x= 1, next_row_x=0 or 2, abs(0-1)==abs(1-2)
+            //                  x= 1
+            //                  /  |  \
+            //    next_row_x = 0   1   2  3<-(Safe~~)
+            //                 X   X   X  Q
+            //      abs()     (0 - 1),(1 - 2)
+            //                 斜左下 , 斜右下
+            else if (Math.abs(queens[next_row_x] - queens[x]) == Math.abs(x - next_row_x)) {
                 return false;
             }
         }
         return true;
     }
 
-    public void addSolution(List<List<String>> res, int[] queens) {
+    static public void addSolution(List<List<String>> res, int[] queens) {
         List<String> list = new ArrayList<>();
         for (int i = 0; i < queens.length; i++) {
             StringBuilder sb = new StringBuilder();
