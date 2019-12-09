1d1c17d647295003e4258e962b1bb05346f198bb
Wei <ha271923@gmail.com>
2019-12-09 23:12:34 +0800

    update

--- app/src/main/java/com/cspirat/SpiralMatrix.java
+++ app/src/main/java/com/cspirat/SpiralMatrix.java
@@ -17,9 +17,9 @@ public class SpiralMatrix {
      Given the following matrix:
 
      [
-     [ 1, 2, 3 ],
-     [ 4, 5, 6 ],
-     [ 7, 8, 9 ]
+       [ 1, 2, 3 ],
+       [ 4, 5, 6 ],
+       [ 7, 8, 9 ]
      ]
      You should return [1,2,3,6,9,8,7,4,5].
 
@@ -29,41 +29,80 @@ public class SpiralMatrix {
      * @param matrix
      * @return
      */
+/*
+     [
+       [ 0, 1, 2 ],
+       [ 7, 8, 3 ],
+       [ 6, 5, 4 ]
+     ]
+           ||
+     [
+       [ (0,0), (0,1), (0,2) ],
+       [ (1,0), (1,1), (1,2) ],
+       [ (2,0), (2,1), (2,2) ],
+     ]
+           ||
+     [
+       [ (0,0), (0,1), (0,2) ],  A. scan X (0 ~ n-1 , 0      )
+       [      ,      , (1,2) ],  B. scan Y (n-1     , 1 ~ n-1)
+       [      ,      , (2,2) ],
+       [ (2,0), (2,1),       ],  C. scan X (n-1 ~ 0 , n-1    )
+       [ (1,0),      ,       ],  D. scan Y (0       , n-1 ~ 1)
+                        ~~~~  繞完一圈  ~~~~
+       [      , (1,1),       ],  scan X (1 ~ n-2 , n-2    )
+     ]
 
-    public List<Integer> spiralOrder(int[][] matrix) {
+ */
+    public static void main(String[] args) {
+        int[][] input = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        List<Integer> ret;
+        ret = spiralOrder(input);
+        System.out.println("ANS: ret="+ret);
+    }
+
+    static public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> res = new ArrayList<>();
         if (matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) {
             return res;
         }
-        int rowBegin = 0;
-        int rowEnd = matrix.length - 1;
-        int colBegin = 0;
-        int colEnd = matrix[0].length - 1;
 
-        while (rowBegin <= rowEnd && colBegin <= colEnd) {
-            for (int i = colBegin; i <= colEnd; i++) {
-                res.add(matrix[rowBegin][i]);
+        int xStart = 0;
+        int xEnd = matrix[0].length - 1;
+        int yStart = 0;
+        int yEnd = matrix.length - 1;
+
+        while (xStart <= xEnd && yStart <= yEnd ) {
+            System.out.println(res);
+            // A. scan X (0 ~ n-1 , 0      )  方向往右
+            for (int x = xStart; x <= xEnd; x++) {
+                res.add(matrix[yStart][x]);
             }
-            rowBegin++;
 
-            for (int i = rowBegin; i <= rowEnd; i++) {
-                res.add(matrix[i][colEnd]);
+            // B. scan Y (n-1     , 1 ~ n-1)  方向往下
+            yStart++; // 從下一列開始
+            for (int y = yStart; y <= yEnd; y++) {
+                res.add(matrix[y][xEnd]);
             }
-            colEnd--;
-
-            if (rowBegin <= rowEnd) {
-                for (int i = colEnd; i >= colBegin; i--) {
-                    res.add(matrix[rowEnd][i]);
+            // C. scan X (n-1 ~ 0 , n-1    )  方向往左
+            xEnd--; //
+            if (yStart <= yEnd) { // KEY: 若是{1,2,3,4}數字方陣, 則掃到
+                for (int x = xEnd; x >= xStart; x--) {
+                    res.add(matrix[yEnd][x]);
                 }
             }
-            rowEnd--;
-
-            if (colBegin <= colEnd) {
-                for (int i = rowEnd; i >= rowBegin; i--) {
-                    res.add(matrix[i][colBegin]);
+            // D. scan Y (0       , n-1 ~ 1)  方向往上
+            yEnd--; //
+            if (xStart <= xEnd) { // KEY: 若數字方陣是{1,2,3,4}時, 則掃到(x=0,y=1)時,無法再往上掃
+                for (int y = yEnd; y >= yStart; y--) {
+                    res.add(matrix[y][xStart]);
                 }
             }
-            colBegin++;
+            // 繞ABCD四邊, 掃完一圈, xStart往右移1
+            xStart++;
         }
 
         return res;
