f0b8279365a2d32fabb02a42e8ec3d5e7ed052b9
ha271923 <ha271923@gmail.com>
2020-01-06 11:51:29 +0800

    update

--- app/src/main/java/com/cspirat/UniquePathsII.java
+++ app/src/main/java/com/cspirat/UniquePathsII.java
@@ -1,5 +1,7 @@
 package com.cspirat;
 
+import com.hawk.leetcode.Basic.DP_utils;
+
 /**
  * Project Name : Leetcode
  * Package Name : leetcode
@@ -20,9 +22,9 @@ public class UniquePathsII {
      There is one obstacle in the middle of a 3x3 grid as illustrated below.
 
      [
-     [0,0,0],
-     [0,1,0],
-     [0,0,0]
+       [0,0,0],
+       [0,1,0],
+       [0,0,0]
      ]
      The total number of unique paths is 2.
 
@@ -32,20 +34,67 @@ public class UniquePathsII {
      * @param obstacleGrid
      * @return
      */
+    public static void main(String[] args) {
 
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int length = obstacleGrid[0].length;
-        int[] res = new int[length];
-        res[0] = 1;
-        for (int i = 0; i < obstacleGrid.length; i++) {
-            for (int j = 0; j < obstacleGrid[0].length; j++) {
-                if (obstacleGrid[i][j] == 1) {
-                    res[j] = 0;
-                } else if (j > 0) {
-                    res[j] += res[j - 1];
-                }
+        int[][] obstacleGrid = new int[3][3];
+        obstacleGrid[1][1] = 1;
+        // int[][] obstacleGrid = new int[7][3];
+        // obstacleGrid[1][1] = 1;
+        // obstacleGrid[2][2] = 1;
+
+        System.out.println(uniquePathsWithObstacles(obstacleGrid));
+        System.out.println(uniquePathsWithObstacles_DP(obstacleGrid));
+    }
+    /*
+      DP[][]儲存著匯流至此的走法有幾種, 每一個位置x,y的走法, 取決於上方可能的走法,與左方可能的走法, 的和
+      因為方向只有, 右與下兩種, 所以最上一列DP[0][x]與作左一行DP[y][0]必定無其他方向匯流至此,僅唯一一種走法
+                width>=w=dp[y][x-1] [y][x]
+      height>=h=dp[y-1][x] |       | A1   |
+                  [y  ][x] |  A2   | A1+A2|
+
+      algorithm = DP[y][x] = A1+X2 = DP[y-1][x]+DP[y][x-1]
+     */
+    // Tips: 多了一個障礙陣列, 只要遇到障礙陣列, 可能走法重設為0
+    // Tips: 與leetcode的uniquePaths_DP程式碼進行比較, 只有一個小差異
+    static public int uniquePathsWithObstacles_DP(int[][] obstacleGrid) {
+        int h = obstacleGrid.length;
+        int w = obstacleGrid[0].length;
+
+        int[][] DP = new int[h][w];
+        // 初始化: 因為方向只有, 右與下兩種, 所以最上一列與作左一行必定無其他方向匯流至此
+        for (int i = 0; i < h; i++) {
+            DP[i][0] = 1;
+        }
+        for (int i = 0; i < w; i++) {
+            DP[0][i] = 1;
+        }
+        // 開始走: algorithm = DP[y][x] = A1+X2 = DP[y-1][x]+DP[y][x-1]
+        for (int y = 1; y < h; y++) { // 掃描y
+            for (int x = 1; x < w; x++) { // 掃描x
+                if (obstacleGrid[y][x] == 1) // KEY: 一旦發現障礙物,則此路不通, 可能走法重設為0
+                    DP[y][x] = 0;
+                else
+                    DP[y][x] = DP[y - 1][x] + DP[y][x - 1]; // 上方可能的走法 + 左方可能的走法
             }
         }
-        return res[length - 1];
+        DP_utils.print(DP);
+        return DP[h - 1][w - 1];
+    }
+
+    static public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int h = obstacleGrid.length;
+        int w = obstacleGrid[0].length;
+        int[] DP = new int[w];
+        DP[0] = 1;
+        for (int y = 0; y < h; y++) {
+            for (int x = 0; x < w; x++) {
+                if (obstacleGrid[y][x] == 1)
+                    DP[x] = 0;
+                else if (x > 0)
+                    DP[x] += DP[x - 1];
+            }
+        }
+        DP_utils.print(DP);
+        return DP[w - 1];
     }
 }
--- app/src/main/java/com/freetymekiyan/algorithms/level/medium/UniquePaths2.java
+++ app/src/main/java/com/freetymekiyan/algorithms/level/medium/UniquePaths2.java
@@ -1,5 +1,7 @@
 package com.freetymekiyan.algorithms.level.medium;
 
+import com.hawk.leetcode.Basic.DP_utils;
+
 /**
  * Follow up for "Unique Paths":
  * <p>
@@ -12,9 +14,9 @@ package com.freetymekiyan.algorithms.level.medium;
  * There is one obstacle in the middle of a 3x3 grid as illustrated below.
  * <p>
  * [
- * [0,0,0],
- * [0,1,0],
- * [0,0,0]
+ *  [0,0,0],
+ *  [0,1,0],
+ *  [0,0,0]
  * ]
  * The total number of unique paths is 2.
  * <p>
@@ -29,6 +31,15 @@ class UniquePaths2 {
         System.out.println(uniquePathsWithObstacles(obstacleGrid));
     }
 
+    /*
+     *           xLen>=x=[0][x] [0][1] [0][2] [0][3]  word2
+     * yLen>=y=dp[y][0] |  2   |  1   |  1   |  0   |
+     *           [1][0] |  1   |  0   |  1   |  0   |
+     *           [2][0] |  1   |  1   |  1   |  1   |
+     *           [3][0] |  0   |  0   |  0   |  0   |
+     *            word1
+     *  Ans: 2
+     * */
     /**
      * DP, bottom-up approach
      * build from end point to start point
@@ -36,19 +47,27 @@ class UniquePaths2 {
      * paths[r][c] = obstacleGrid[r][c] == 1 ? 0
      * : paths[r + 1][c] + paths[r][c + 1];
      */
+    // Q: Now consider if some obstacles are added to the grids. How many unique paths would there be?
+    //    障礙物可以多個, 初始路線棋譜請自行輸入int[][] obstacleGrid, 數字1代表障礙,出發(0,0)->目的(X,Y)
     public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        if (obstacleGrid == null) return 0;
-        int m = obstacleGrid.length;
-        if (m == 0) return 0;
-        int n = obstacleGrid[0].length;
-        int[][] paths = new int[m + 1][n + 1];
+        if (obstacleGrid == null)
+            return 0;
+        int y = obstacleGrid.length;
+        if (y == 0)
+            return 0;
+        int x = obstacleGrid[0].length;
+        int[][] dp = new int[y + 1][x + 1];
 
-        paths[m - 1][n] = 1;
-        for (int r = m - 1; r >= 0; r--) {
-            for (int c = n - 1; c >= 0; c--) {
-                paths[r][c] = obstacleGrid[r][c] == 1 ? 0 : paths[r + 1][c] + paths[r][c + 1];
+        dp[y - 1][x] = 1;
+        for (int r = y - 1; r >= 0; r--) {
+            for (int c = x - 1; c >= 0; c--) {
+                if(obstacleGrid[r][c] == 1 )
+                    dp[r][c] = 0;
+                else
+                    dp[r][c] = dp[r + 1][c] + dp[r][c + 1]; // KEY:
             }
         }
-        return paths[0][0];
+        DP_utils.print(dp);
+        return dp[0][0];
     }
 }
