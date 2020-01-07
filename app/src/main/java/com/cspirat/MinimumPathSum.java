8dd7db257d9015866d18665ed836bf8aa48fcde4
ha271923 <ha271923@gmail.com>
2020-01-08 00:21:55 +0800

    update

--- app/src/main/java/com/cspirat/MinimumPathSum.java
+++ app/src/main/java/com/cspirat/MinimumPathSum.java
@@ -1,5 +1,7 @@
 package com.cspirat;
 
+import com.hawk.leetcode.Basic.DP_utils;
+
 /**
  * Project Name : Leetcode
  * Package Name : leetcode
@@ -27,16 +29,45 @@ public class MinimumPathSum {
      * @param grid
      * @return
      */
-    public int minPathSum(int[][] grid) {
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (i == 0 && j != 0) grid[i][j] += grid[i][j - 1];
-                if (i != 0 && j == 0) grid[i][j] += grid[i - 1][j];
-                if (i != 0 && j != 0) {
-                    grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+    /*
+        Input:
+        [
+          [1,3,1],
+          [1,5,1],
+          [4,2,1]
+        ]
+     */
+    public static void main(String[] args) {
+        int[][] input =
+        { { 1 , 3 , 1 },
+          { 1 , 5 , 1 },
+          { 4 , 2 , 1 } };
+
+        int[][] input2 =
+        { { 1, 2, 3 },
+          { 4, 5, 6 },
+          { 7, 8, 9 } };
+        System.out.println(minPathSum1(input));
+    }
+
+    static public int minPathSum1(int[][] grid) {
+        int[][] DP = grid;
+        int W = DP[0].length;
+        int H = DP.length;
+        // 暴力法, 把每一格答案掃出來
+        for (int y = 0; y < H; y++) {
+            for (int x = 0; x < W; x++) {
+                if (y == 0 && x != 0) // 往右走到底,除[0][0]以外
+                    DP[0][x] += DP[0][x - 1];
+                if (y != 0 && x == 0) // 往下走到底,除[0][0]以外
+                    DP[y][0] += DP[y - 1][0];
+
+                if (y != 0 && x != 0) { // 從上往下走 或 從左往右走 哪個較近?
+                    DP[y][x] += Math.min(DP[y - 1][x], DP[y][x - 1]);
                 }
             }
         }
-        return grid[grid.length - 1][grid[0].length - 1];
+        DP_utils.print(DP);
+        return DP[H - 1][W - 1]; // 右下角
     }
 }
