d65033c51a7fe242800e7bc7eedd1ab9ddb83d5c
ha271923 <ha271923@gmail.com>
2020-02-09 19:20:30 +0800

    update

--- app/src/main/java/com/cspirat/SimplifyPath.java
+++ app/src/main/java/com/cspirat/SimplifyPath.java
@@ -11,6 +11,12 @@ import java.util.Stack;
  * Description : 71. Simplify Path
  */
 public class SimplifyPath {
+    public static void main(String[] args) {
+        // String input="/home/"; //  Ans: "/home"
+        String input="/a/./b/../../c/"; // Ans: "/c"
+        System.out.println(simplifyPath(input));
+    }
+
     /**
      * Given an absolute path for a file (Unix-style), simplify it.
 
@@ -25,23 +31,35 @@ public class SimplifyPath {
      * @param path
      * @return
      */
-    public String simplifyPath(String path) {
+    // Feature requirement:
+    // 1. remove last '/' symbol
+    // 2. current path symbol "/."
+    // 3. path delete symbol "/.."
+    // 路徑題, 大多是用stack去解:
+    static public String simplifyPath(String path) {
         Stack<String> stack = new Stack<>();
-        String[] paths = path.split("/+");
-        for (String s : paths) {
-            if (s.equals("..")) {
+
+        String[] paths = path.split("/+"); // Tips: split()會把'/'當成區隔符號,把區隔出來的字串們存到path陣列
+        // before split = "/a/./b/../../c/"
+        //                [0][1] [2] [3] [4]  [5]  [6]
+        // after  split = ""+"a"+"."+"b"+".."+".."+"c"
+        for (String s : paths) { // LOOP: 過濾出標準字串路徑
+            if (s.equals("..")) { // 路徑回上一層
                 if (!stack.isEmpty()) {
-                    stack.pop();
+                    stack.pop(); // 回上一層
                 }
-            } else if (!s.equals(".") && !s.equals("")) {
-                stack.push(s);
+            } else if (!s.equals(".") && !s.equals("")) { // 標準路徑字串
+                stack.push(s); // 路徑往下一層
             }
+            // 其他情況,維持現有路徑
         }
+
         String res = "";
-        while (!stack.isEmpty()) {
+        while (!stack.isEmpty()) { // LOOP: 路徑全部接起來
             res = "/" + stack.pop() + res;
         }
-        if (res.length() == 0) {
+
+        if (res.length() == 0) { // 若都沒有路徑, 返回Root
             return "/";
         }
         return res;
