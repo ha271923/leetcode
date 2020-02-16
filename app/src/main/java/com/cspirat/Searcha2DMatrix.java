94b0496aa0433708475f4c23f13926a0881f450c
ha271923 <ha271923@gmail.com>
2020-02-16 23:49:48 +0800

    update

--- app/src/main/java/com/cspirat/Searcha2DMatrix.java
+++ app/src/main/java/com/cspirat/Searcha2DMatrix.java
@@ -9,6 +9,17 @@ package com.cspirat;
  * Description : 74. Search a 2D Matrix
  */
 public class Searcha2DMatrix {
+    public static void main(String[] args) {
+        int[][] input =
+        {
+          {1,   3,  5,  7},
+          {10, 11, 16, 20},
+          {23, 30, 34, 50}
+        };
+        int target = 3;
+        boolean ret = searchMatrix(input, target);
+        System.out.println("ret=" + ret);
+    }
     /**
      * Write an efficient algorithm that searches for a value in an m x n matrix.
      * This matrix has the following properties:
@@ -35,20 +46,35 @@ public class Searcha2DMatrix {
      * @param target
      * @return
      */
-    public boolean searchMatrix(int[][] matrix, int target) {
+    static public boolean searchMatrix(int[][] matrix, int target) {
         if (matrix == null || matrix.length == 0) return false;
         int row = matrix.length;
         int col = matrix[0].length;
-        int begin = 0, end = row * col - 1;
-        while (begin <= end) {
-            int mid = (end - begin) / 2 + begin;
-            int value = matrix[mid / col][mid % col];
-            if (value == target) {
+        int beginIdx = 0;
+        int endIdx = row * col - 1; //  將2D陣列轉換成 1D陣列,最後一個元素idx
+
+        // 用1D搜尋
+        while (beginIdx <= endIdx) {
+            int midIdx = (endIdx - beginIdx) / 2 + beginIdx;
+            /*
+             *              2D array                  ==>                1D array
+             *     x0     x1     x2     x3    col=4
+             * y0|  1beg|  3   |  5   |  7   |              b             m                  e
+             * y1| 10   | 11mid| 16   | 20   |        ==> [ 1, 3, 5, 7,10,11,16,20,23,30,34,50]
+             * y2| 23   | 30   | 34   | 50end|            |   col=4   |   col=4   |   col=4   | 數組可切三段
+             * raw=3
+             * midIdx=(11-0)/2+0=5
+             */
+            System.out.println("midIdx="+ midIdx+"  col="+col);
+            int midVal = matrix[midIdx / col][midIdx % col]; // KEY: 1D轉2D取中間值值 algorithm
+            // 比對target
+            // begin ---------- midVal -------------- end
+            if (midVal == target) {
                 return true;
-            } else if (value < target) {
-                begin = mid + 1;
-            } else {
-                end = mid - 1;
+            } else if (midVal < target) { // 中值 小於 目標值, 搜尋區段為中值以右 區間
+                beginIdx = midIdx + 1;
+            } else {  // 中值 大於 目標值, 搜尋區段為中值以左 區間
+                endIdx = midIdx - 1;
             }
         }
         return false;
