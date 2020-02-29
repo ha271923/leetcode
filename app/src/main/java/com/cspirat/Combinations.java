ae764705fb95ec7b51b3dde3dcbf7323dafafa61
ha271923 <ha271923@gmail.com>
2020-02-29 21:41:49 +0800

    Combinations

--- app/src/main/java/com/cspirat/Combinations.java
+++ app/src/main/java/com/cspirat/Combinations.java
@@ -12,46 +12,79 @@ import java.util.List;
  * Description : TODO
  */
 public class Combinations {
-
+    public static void main(String[] args) {
+        List<List<Integer>> res = combine( 4, 2);
+        for (List<Integer> l : res)
+            System.out.println(l);
+    }
     /**
      * 77. Combinations
-     * Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+     * Given two integers nums and count, return all possible combinations of count numbers out of 1 ... nums.
 
      For example,
-     If n = 4 and k = 2, a solution is:
+     If nums = 4 and count = 2, a solution is:
 
      [
-     [2,4],
-     [3,4],
-     [2,3],
-     [1,2],
-     [1,3],
-     [1,4],
+        [2,4],
+        [3,4],
+        [2,3],
+        [1,2],
+        [1,3],
+        [1,4],
      ]
-     [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]
 
-     time : O(n^min{k,n-k})
-     space : O(n);
+     ANS:
+     [[1,2],[1,3],[1,4],
+      [2,3],[2,4],
+     [3,4]]
+
+     time : O(nums^min{count,nums-count})
+     space : O(nums);
      http://stackoverflow.com/questions/31120402/complexity-when-generating-all-combinations
-     * @param n
-     * @param k
+     * @param nums
+     * @param count
      * @return
      */
 
-    public List<List<Integer>> combine(int n, int k) {
+    static public List<List<Integer>> combine(int nums, int count) {
         List<List<Integer>> res = new ArrayList<>();
-        helper(res, new ArrayList<>(), n, k, 1);
+        dfs_recur(res, new ArrayList<>(), nums, count, 1);
         return res;
     }
+    /*
+       If nums = 4 and count = 2, a solution is:
+             LOOP1    LOOP2
+                 count-1
+                 shift+1
+                       |--> [1,2]  dfs_recur(nums,count-1,shift+1)
+               |---[1]<+--> [1,3]  dfs_recur(nums,count-1,shift+1)
+               |       |--> [1,4]  dfs_recur(nums,count-1,shift+1)
+               |
+               |
+               |---[2]<+--> [2,3]  dfs_recur(nums,count-1,shift+1)
+               |       |--> [2,4]  dfs_recur(nums,count-1,shift+1)
+  {1,2,3,4}    |
+               |
+               |---[3]<+
+               |       |--> [3,4]  dfs_recur(nums,count-1,shift+1)
+               |
+               |
+               |
+               |---[4]<+ BREAK! since (i+1 > nums)
 
-    public void helper(List<List<Integer>> res, List<Integer> list, int n, int k, int start) {
-        if (k == 0) {
+
+
+     */
+
+    // 不使用visited Flag 的DFS, 注意這題的答案規則, 很規律
+    static public void dfs_recur(List<List<Integer>> res, List<Integer> list, int nums, int count, int shift) {
+        if (count == 0) {
             res.add(new ArrayList<>(list));
             return;
         }
-        for (int i = start; i <= n; i++) {
+        for (int i = shift; i <= nums; i++) {
             list.add(i);
-            helper(res, list, n, k - 1, i + 1);
+            dfs_recur(res, list, nums, count-1, i+1); // KEY: 注意此處的輸入參數
             list.remove(list.size() - 1);
         }
     }
