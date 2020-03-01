d5147f9d5f0b13db80e714d8a464efb8f8843f50
ha271923 <ha271923@gmail.com>
2020-03-01 19:28:11 +0800

    update

--- app/src/main/java/com/cspirat/Subsets.java
+++ app/src/main/java/com/cspirat/Subsets.java
@@ -7,6 +7,12 @@ import java.util.List;
  * Created by Edward on 28/07/2017.
  */
 public class Subsets {
+    public static void main(String[] args) {
+        int[] inputs = {1,2,3};
+        List<List<Integer>> res = subsets(inputs);
+        System.out.println(res);
+    }
+
     /**
      * 78. Subsets
      * Given a set of distinct integers, nums, return all possible subsets.
@@ -16,14 +22,14 @@ public class Subsets {
      For example,
      If nums = [1,2,3], a solution is:
      [
-     [3],
-     [1],
-     [2],
-     [1,2,3],
-     [1,3],
-     [2,3],
-     [1,2],
-     []
+       [3],
+       [1],
+       [2],
+       [1,2,3],
+       [1,3],
+       [2,3],
+       [1,2],
+       []
      ]
 
      test : [1,2,3]
@@ -52,16 +58,19 @@ public class Subsets {
      */
     public static List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        if (nums == null || nums.length == 0) return res;
-        helper(res, new ArrayList<>(), nums, 0);
+        if (nums == null || nums.length == 0)
+            return res;
+
+        DFS_recur(res, new ArrayList<>(), nums, 0);
         return res;
     }
-    public static void helper(List<List<Integer>> res, List<Integer> list, int[] nums, int index) {
-        res.add(new ArrayList<>(list));
-        for (int i = index; i < nums.length; i++) {
-            list.add(nums[i]);
-            helper(res, list, nums, i + 1);
-            list.remove(list.size() - 1);
+
+    public static void DFS_recur(List<List<Integer>> res, List<Integer> answer, int[] nums, int shift) {
+        res.add(new ArrayList<>(answer));  // 加一組答案到res
+        for (int i = shift; i < nums.length; i++) {
+            answer.add(nums[i]); // PUSH: 每次進入遞迴前, ans加一個數字元
+            DFS_recur(res, answer, nums, i + 1); // KEY: 從答案觀察出PUSH/POP與shift規則
+            answer.remove(answer.size() - 1); // POP: 每次退出遞迴後, ans退一個數字元
         }
     }
 }
