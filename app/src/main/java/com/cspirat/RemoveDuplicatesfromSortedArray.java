afa16841040654df66f826285cd298971bdb6650
ha271923 <ha271923@gmail.com>
2020-03-02 23:53:20 +0800

    update

--- app/src/main/java/com/cspirat/RemoveDuplicatesfromSortedArray.java
+++ app/src/main/java/com/cspirat/RemoveDuplicatesfromSortedArray.java
@@ -3,7 +3,14 @@ package com.cspirat;
 /**
  * Created by Edward on 25/07/2017.
  */
+//   Do not allocate extra space for another array, you must do this in place with constant memory.
 public class RemoveDuplicatesfromSortedArray {
+    public static void main(String[] args) {
+        // test1
+        int[] inputs = {1,1,2,2,3,4,5,6};
+        int res = removeDuplicates(inputs);
+        System.out.println(res);
+    }
     /**
      * 26. Remove Duplicates from Sorted Array
      * Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
@@ -27,14 +34,19 @@ public class RemoveDuplicatesfromSortedArray {
      * @param nums
      * @return
      */
+    // Tips: Sorted Array一路向東掃
+    // Q: Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory
+    // Output有兩個, 1.length   2. int[] nums 這是call-by-reference
     public static int removeDuplicates(int[] nums) {
-        if (nums == null || nums.length == 0) return 0;
-        int count = 1;
+        if (nums == null || nums.length == 0)
+            return 0;
+
+        int newLen = 1;
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i - 1] != nums[i]) {
-                nums[count++] = nums[i];
+            if (nums[i-1] != nums[i]) { // Sorted Array 的前後關希
+                nums[newLen++] = nums[i]; // KEY:
             }
         }
-        return count;
+        return newLen;
     }
 }
