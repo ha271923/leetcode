b4df376b695808588e106759a0e5924d81e4df1c
ha271923 <ha271923@gmail.com>
2020-06-15 17:15:03 +0800

    MergeSort

--- app/src/main/java/com/hawk/leetcode/Basic/Sort/MergeSort.java
+++ app/src/main/java/com/hawk/leetcode/Basic/Sort/MergeSort.java
@@ -1,18 +1,42 @@
 package com.hawk.leetcode.Basic.Sort;
 
 public class MergeSort {
+/*
+A. 先對半分組
+    |3 5 8 1 2 9 4 7 6              |  一開始
+
+    |               |               |  首次對半分
+
+    |       |       |       |       |  再次對半分
+
+    |   |   |   |   |   |   |   |   |  又再次對半分
+
+    |3|5|8|1|2|9|4|7|6| | | | | | |  又又再次對半分, 直到每組都是2個元素
+
+
+B. 分好組後的各組內的2個元素比大小
+    |3|5|8|1|2|9|4|7|6|
+    |3 5|1 8|2 9|4|7|6|  第一次比大小, 掃描區間內(n<=2^1)所有數值
+    |       |   |     |
+    |1 3 5 8|2 9|4 6 7|  區間內再次比大小, 掃描區間內(n<=(2^2))所有數值
+    |       |         |
+    |       |2 4 6 7 9|  區間內又再次比大小, 掃描區間內(n<=(2^3))所有數值
+
+    |1 2 3 4 5 6 7 8 9|  區間內又再次比大小, 掃描區間內(n<=(2^3))所有數值
+ */
+
 
     public static void main(String[] args) {
         int n = 9;
         int[] numbers = null;
         numbers = new int[]{3, 5, 8, 1, 2, 9, 4, 7, 6};
         // Utils.getRandNumbers(numbers); // Random it!
-        mergeSort(numbers, numbers.length);
-        System.out.print(numbers);
-        System.out.println();
+        mergeSort_recursive(numbers, numbers.length);
+        mergeSort_loop(numbers);
+        printArray("Result= ", numbers);
     }
 
-    public static void mergeSort(int[] nums, int len) {
+    public static void mergeSort_recursive(int[] nums, int len) {
         if (len < 2) {
             return;
         }
@@ -21,18 +45,21 @@ public class MergeSort {
         int[] Lnums = new int[mid];
         int[] Rnums = new int[len - mid];
 
-        // copy numbers to L numbers
+        // 數組分兩半
         for (int i = 0  ; i < mid; i++) {
             Lnums[i] = nums[i];
         }
-        // copy numbers to R numbers
         for (int i = mid; i < len; i++) {
             Rnums[i - mid] = nums[i];
         }
-        mergeSort(Lnums, mid); // recursive to separate L numbers
-        mergeSort(Rnums, len - mid); // recursive to separate R numbers
 
+        mergeSort_recursive(Lnums, mid); // 遞迴：數組分兩半
+        mergeSort_recursive(Rnums, len - mid); // 遞迴：數組分兩半
+
+        // 因為 (len <2 ) 所以不會再往下遞迴
+        printArray("merge ++ " , nums);
         merge(nums, Lnums, Rnums, mid, len - mid); // mid=L ~ R=(len-mid)
+        printArray("merge -- " , nums);
     }
 
 /*
@@ -53,20 +80,58 @@ public class MergeSort {
         int Lidx = 0, Ridx = 0, numsIdx = 0;
         //
         while (Lidx < L && Ridx < R) { // 一開始LR都有數值群
-            if (Lnums[Lidx] <= Rnums[Ridx]) { // 誰小誰填入array, 因為是上升排列
+            if (Lnums[Lidx] <= Rnums[Ridx]) { // KEY: 誰小誰填入nums[] OUTPUT array, 因為是上升排列
                 nums[numsIdx++] = Lnums[Lidx++];
             } else {
                 nums[numsIdx++] = Rnums[Ridx++];
             }
         }
-        // R數值群都分完了, L的數值群還有剩下的數
+        // R數值群都分完了, L的數值群還有剩下的已排序過數
         while (Lidx < L) {
             nums[numsIdx++] = Lnums[Lidx++];
         }
-        // L數值群都分完了, R的數值群還有剩下的數
+        // L數值群都分完了, R的數值群還有剩下的已排序過數
         while (Ridx < R) {
             nums[numsIdx++] = Rnums[Ridx++];
         }
 
     }
+
+
+    public static void printArray(String TAG, int[] nums){
+        System.out.print(TAG);
+        for(int n:nums){
+            System.out.print(n+",");
+        }
+        System.out.println();
+    }
+
+
+
+    public static void mergeSort_loop(int[] nums) {
+        int[] orderedArr = new int[nums.length];
+        for (int i = 2; i < nums.length * 2; i *= 2) {
+            for (int j = 0; j < (nums.length + i-1) / i; j++) {
+                int L = i * j;
+                int mid = L + i / 2 >= nums.length ? (nums.length - 1) : (L + i / 2);
+                int R = i * (j + 1) - 1 >= nums.length ? (nums.length - 1) : (i * (j + 1) - 1);
+                int start = L, l = L, m = mid;
+
+                while (l < mid && m <= R) {
+                    if (nums[l] < nums[m]) {
+                        orderedArr[start++] = nums[l++];
+                    } else {
+                        orderedArr[start++] = nums[m++];
+                    }
+                }
+                while (l < mid)
+                    orderedArr[start++] = nums[l++];
+                while (m <= R)
+                    orderedArr[start++] = nums[m++];
+
+                System.arraycopy(orderedArr, L, nums, L, R - L + 1);
+            }
+        }
+    }
+
 }
