6019b6b1d42c4dd353645f849706712244746fd0
ha271923 <ha271923@gmail.com>
2020-06-15 18:33:26 +0800

    update

--- app/src/main/java/com/cspirat/ScrambleString.java
+++ app/src/main/java/com/cspirat/ScrambleString.java
@@ -10,6 +10,12 @@ package com.cspirat;
  */
 public class ScrambleString {
 
+    public static void main(String[] args) {
+        // String s1 = "great", s2 = "rgeat"; // T
+        // String s1 = "abcde", s2 = "caebd"; // F
+        String s1 = "abcdefghijklmnopqrstuvwxyz", s2 = "zyxwvutsrqponmlkjihgfedcba"; //
+        boolean ret = isScramble(s1,s2);
+    }
     /**
 
      time : O(n!)
@@ -19,26 +25,40 @@ public class ScrambleString {
      * @param s2
      * @return
      */
+    // https://coordinate.wang/index.php/archives/2108/
+    static public boolean isScramble(String s1, String s2) {
 
-    public boolean isScramble(String s1, String s2) {
-        if (s1 == null || s2 == null) return false;
-        if (s1.equals(s2)) return true;
-
+        if (s1 == null || s2 == null)
+            return false;
+        if (s1.equals(s2))
+            return true;
+        // 字母計數陣列
         int[] letters = new int[26];
         int len = s1.length();
         for (int i = 0; i < len; i++) {
             letters[s1.charAt(i) - 'a']++;
-            letters[s2.charAt(i) - 'a']--;
+            letters[s2.charAt(i) - 'a']--; // 可以是負數
         }
+
         for (int i = 0; i < 26; i++) {
-            if (letters[i] != 0) return false;
+            if (letters[i] != 0)
+                return false;
         }
+
+        // 遞迴
         for (int i = 1; i < len; i++) {
             if (isScramble(s1.substring(0, i), s2.substring(0, i))
-                    && isScramble(s1.substring(i), s2.substring(i))) return true;
+                    && isScramble(s1.substring(i), s2.substring(i)))
+                return true;
             if (isScramble(s1.substring(0, i), s2.substring(len - i))
-                    && isScramble(s1.substring(i), s2.substring(0, len - i))) return true;
+                    && isScramble(s1.substring(i), s2.substring(0, len - i)))
+                return true;
         }
         return false;
     }
+
+   // 这道题也可以用动态规划 Dynamic Programming，
+    // 根据以往的经验来说，根字符串有关的题十有八九可以用 DP 来做，那么难点就在于如何找出状态转移方程
+
+
 }
