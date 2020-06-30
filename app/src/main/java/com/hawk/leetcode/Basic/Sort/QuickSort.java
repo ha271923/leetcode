15c2ab2c8debf51b8b549c790987c8eadebe73e5
ha271923 <ha271923@gmail.com>
2020-06-30 17:40:23 +0800

    update

--- app/src/main/java/com/hawk/leetcode/Basic/Sort/QuickSort.java
+++ app/src/main/java/com/hawk/leetcode/Basic/Sort/QuickSort.java
@@ -1,5 +1,7 @@
 package com.hawk.leetcode.Basic.Sort;
 
+import com.utils.Out;
+
 import java.util.Random;
 
 public class QuickSort {
@@ -12,16 +14,17 @@ public class QuickSort {
         numbers = new int[] { 3 ,5 ,8 ,1 ,2 ,9 ,4 ,7 ,6 };
         // Utils.getRandNumbers(numbers); // Random it!
         quick(numbers);
+        // quicksort_hawk(numbers, 0 , 0);
         System.out.print(numbers);
         System.out.println();
     }
 
     public static void quick(int[] numbers)
     {
-        Sort(numbers, 0, numbers.length - 1);
+        quickSort(numbers, 0, numbers.length - 1);
     }
 
-    private static void Sort(int[] numbers, int left, int right)
+    private static void quickSort(int[] numbers, int left, int right)
     {
         if (left < right)
         {
@@ -35,7 +38,9 @@ public class QuickSort {
             // 3. random
             int seed=0;
             Random r = new Random();
-            seed = r.nextInt((right - left) + 1) + left;
+            //  0        L  p R
+            //  |          | |           |
+            seed = r.nextInt((right - left) + 1) + left; // API: if seed = r.nextInt(10);  seed = 0~9
             pivot = numbers[seed];
 
             int L = left - 1;  // 一開始在界外
@@ -53,15 +58,26 @@ public class QuickSort {
                 Swap(numbers, L, R); // 因為掃到LR均不, 所以互換
             }
             // |left->| ..左半部數值群.. |(退回++L前的位置才是小於pivot)| pivot |right->| ..右半部數值群.. |(退回--R前的位置才是大於pivot)|
-            Sort(numbers, left, --L); // 左半都是小於Pivot: left ~ (退還上面的++L)
-            Sort(numbers, ++R, right); // 右半都是大於Pivot: (退還上面的--R) ~ right
+            quickSort(numbers, left, --L); // 左半都是小於Pivot: left ~ (退還上面的++L)
+            quickSort(numbers, ++R, right); // 右半都是大於Pivot: (退還上面的--R) ~ right
         }
     }
 
     private static void Swap(int[] numbers, int i, int j)
     {
-        int number = numbers[i];
+        int tmp = numbers[i];
         numbers[i] = numbers[j];
-        numbers[j] = number;
+        numbers[j] = tmp;
+    }
+
+
+    static public void quicksort_hawk(int[] nums, int left, int right){
+        Random r = new Random();
+        int seed = 0;
+        for(int i=0; i<100; i++ ) {
+            seed = r.nextInt(10);
+            Out.i("i="+i+"  seed="+seed);
+        }
+
     }
 }
