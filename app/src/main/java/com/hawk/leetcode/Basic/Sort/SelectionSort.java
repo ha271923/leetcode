191ea7ad18a24691976677e1ddc32227cdd0f283
ha271923 <ha271923@gmail.com>
2020-10-08 10:50:02 +0800

    Sort: selectionSort

--- app/src/main/java/com/hawk/leetcode/Basic/Sort/SelectionSort.java
+++ app/src/main/java/com/hawk/leetcode/Basic/Sort/SelectionSort.java
@@ -1,7 +1,7 @@
 package com.hawk.leetcode.Basic.Sort;
 
 public class SelectionSort {
-    // KEY: 簡單！ 找到最小跟當前第1位互換, 重複, 直到全部數處理完
+    // KEY: 簡單！ 每次將所有未排序過的n張牌, 全部掃一遍, 找到最小跟當前第1位互換, 重複(n-1)張牌, 直到全部數處理完
     public static void main(String[] args) {
         int[] numbers;
         numbers = new int[] { 3 ,5 ,8 ,1 ,2 ,9 ,4 ,7 ,6 };
@@ -16,13 +16,13 @@ public class SelectionSort {
 
     // https://zh.wikipedia.org/wiki/%E9%80%89%E6%8B%A9%E6%8E%92%E5%BA%8F#Java
     static void selectionSort(int[] arr) {
-        for (int baseIdx = 0; baseIdx < arr.length - 1; baseIdx++) {
+        for (int baseIdx = 0; baseIdx < arr.length - 1; baseIdx++) { // LOOP1: pic num1
             int minIdx = baseIdx;
-            for (int selectIdx = baseIdx + 1; selectIdx < arr.length; selectIdx++) {
-                if (arr[selectIdx] < arr[minIdx])
+            for (int selectIdx = baseIdx + 1; selectIdx < arr.length; selectIdx++) { // LOOP2: pick num2
+                if (arr[selectIdx] < arr[minIdx]) // compare with min num, 誰才是最小
                     minIdx = selectIdx;
             }
-            Swap(arr, baseIdx, minIdx);
+            Swap(arr, baseIdx, minIdx); // 找到就交換!
         }
     }
 
