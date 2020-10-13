5944abf2c00abae6240b9c94fbf730079b4b5a61
ha271923 <ha271923@gmail.com>
2020-10-13 13:11:55 +0800

    Sort: mergeSort patch1

--- app/src/main/java/com/hawk/leetcode/Basic/Sort/MergeSort.java
+++ app/src/main/java/com/hawk/leetcode/Basic/Sort/MergeSort.java
@@ -2,7 +2,9 @@ package com.hawk.leetcode.Basic.Sort;
 
 public class MergeSort {
 /*
-A. 先對半分組
+https://alrightchiu.github.io/SecondRound/comparison-sort-merge-sorthe-bing-pai-xu-fa.html
+
+A. 先對半分組 ( Divid )
     |3 5 8 1 2 9 4 7 6              |  一開始
 
     |               |               |  首次對半分
@@ -16,12 +18,13 @@ A. 先對半分組
 
 B. 分好組後的各組內的2個元素比大小
     |3|5|8|1|2|9|4|7|6|
+    |   |   |   |   | |
     |3 5|1 8|2 9|4|7|6|  第一次比大小, 掃描區間內(n<=2^1)所有數值
-    |       |   |     |
+    |       |   |     |  sort1, merge1
     |1 3 5 8|2 9|4 6 7|  區間內再次比大小, 掃描區間內(n<=(2^2))所有數值
-    |       |         |
+    |       |         |  sort2, merge2
     |       |2 4 6 7 9|  區間內又再次比大小, 掃描區間內(n<=(2^3))所有數值
-
+    |                 |  sort3, merge3
     |1 2 3 4 5 6 7 8 9|  區間內又再次比大小, 掃描區間內(n<=(2^3))所有數值
  */
 
@@ -31,12 +34,14 @@ B. 分好組後的各組內的2個元素比大小
         int[] numbers = null;
         numbers = new int[]{3, 5, 8, 1, 2, 9, 4, 7, 6};
         // Utils.getRandNumbers(numbers); // Random it!
-        mergeSort_recursive(numbers, numbers.length);
+        mergeSort_recursive(numbers);
         mergeSort_loop(numbers);
         printArray("Result= ", numbers);
     }
 
-    public static void mergeSort_recursive(int[] nums, int len) {
+    public static void mergeSort_recursive(int[] nums) {
+        int len = nums.length;
+
         if (len < 2) {
             return;
         }
@@ -45,7 +50,7 @@ B. 分好組後的各組內的2個元素比大小
         int[] Lnums = new int[mid];
         int[] Rnums = new int[len - mid];
 
-        // 數組分兩半
+        // 手工將數組分兩半, nums[] to Lnums[],Rnums[]
         for (int i = 0  ; i < mid; i++) {
             Lnums[i] = nums[i];
         }
@@ -53,12 +58,13 @@ B. 分好組後的各組內的2個元素比大小
             Rnums[i - mid] = nums[i];
         }
 
-        mergeSort_recursive(Lnums, mid); // 遞迴：數組分兩半
-        mergeSort_recursive(Rnums, len - mid); // 遞迴：數組分兩半
+        mergeSort_recursive(Lnums); // 遞迴：數組分兩半
+        mergeSort_recursive(Rnums); // 遞迴：數組分兩半
 
         // 因為 (len <2 ) 所以不會再往下遞迴
         printArray("merge ++ " , nums);
-        merge(nums, Lnums, Rnums, mid, len - mid); // mid=L ~ R=(len-mid)
+        // 叫merge sort的原因, 是因為一開始先分支下去變成一個元素一個節點, 接著每層需將排好的Lnums與Rnums進行merge成節點, 最終merge回一個nums[]
+        merge(nums, Lnums, Rnums); // Sort two array and merge it to nums in this function
         printArray("merge -- " , nums);
     }
 
@@ -75,28 +81,28 @@ B. 分好組後的各組內的2個元素比大小
     // Tips: L(邊界)至少會從1開始, 不是0
     // Tips: L代表此次二分的左邊界起點, R代表此次二分的右邊界終點
     // Tips: 不須用swap, 因為我們有三個array
-    public static void merge(int[] nums, int[] Lnums, int[] Rnums, int L, int R) {
+    public static void merge(int[] nums, int[] Lnums, int[] Rnums) {
+        int L = Lnums.length;
+        int R = Rnums.length;
 
         int Lidx = 0, Ridx = 0, numsIdx = 0;
         //
         while (Lidx < L && Ridx < R) { // 一開始LR都有數值群
-            if (Lnums[Lidx] <= Rnums[Ridx]) { // KEY: 誰小誰填入nums[] OUTPUT array, 因為是上升排列
-                nums[numsIdx++] = Lnums[Lidx++];
+            if (Lnums[Lidx] <= Rnums[Ridx]) { // KEY: sorting, 誰小誰填入nums[] OUTPUT array, 因為是上升排列
+                nums[numsIdx++] = Lnums[Lidx++]; // merging
             } else {
-                nums[numsIdx++] = Rnums[Ridx++];
+                nums[numsIdx++] = Rnums[Ridx++]; // merging
             }
         }
         // R數值群都分完了, L的數值群還有剩下的已排序過數
-        while (Lidx < L) {
+        while (Lidx < L) { // merging
             nums[numsIdx++] = Lnums[Lidx++];
         }
         // L數值群都分完了, R的數值群還有剩下的已排序過數
-        while (Ridx < R) {
+        while (Ridx < R) { // merging
             nums[numsIdx++] = Rnums[Ridx++];
         }
-
     }
-
 
     public static void printArray(String TAG, int[] nums){
         System.out.print(TAG);
@@ -105,8 +111,6 @@ B. 分好組後的各組內的2個元素比大小
         }
         System.out.println();
     }
-
-
 
     public static void mergeSort_loop(int[] nums) {
         int[] orderedArr = new int[nums.length];
