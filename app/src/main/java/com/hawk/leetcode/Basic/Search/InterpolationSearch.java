8359e4e23b68d2499c7b6841c5e2cae2ced9d259
ha271923 <ha271923@gmail.com>
2020-10-18 23:40:13 +0800

    Search: Interpolation Search

--- app/src/main/java/com/hawk/leetcode/Basic/Search/InterpolationSearch.java
+++ app/src/main/java/com/hawk/leetcode/Basic/Search/InterpolationSearch.java
@@ -4,11 +4,15 @@ import com.utils.Out;
 
 public class InterpolationSearch {
     /*
-      內插公式:
-      x = [(y-y1)*(x2-x1)]/[(y2-y1)+x1]
+      在已排序的資料中，將資料視為相同斜率的點，藉由在線上的移動來尋找我們需要中間的索引並進行資料比對，
+      如果取出的值小於要尋找的值，則提高下界，
+      如果取出的值大於要尋找的 值，則降低下界，
+      如此不斷的減少搜尋的範圍，所以其本原則與二分搜尋法是相同的，至於中間值的尋找是透過比例運算
 
       內插曲線:
+      插值 = (設算數 - 最小數) / (最大數 - 最小數)
       https://magiclen.org/interpolation-search/
+      https://ithelp.ithome.com.tw/articles/10207069
      */
     public static void main(String[] args) {
         // 0  1  2  3   4   5   6   7   8   9  10  11  12
@@ -22,25 +26,30 @@ public class InterpolationSearch {
         System.out.println();
     }
 
-    public static int interpolationSearch(final int[] array, final int targetElement) {
-        int start = 0;
-        int end = array.length - 1;
+    public static int interpolationSearch(final int[] array, final int y) {
+        int x1 = 0;
+        int x2 = array.length - 1;
 
-        while (end >= start) {
-            final int x = (targetElement - array[start]) * (end - start) / (array[end] - array[start]) + start;
+        while (x2 >= x1) { // LOOP:
+            //   內插公式:
+            //      將首點表示為(x1,y1)，將尾點表示為(x2,y2)，(x,y)為位於中間相同斜率的點, 求(x,y)點公式為：
+            //      (y-y1)/(x-x1) =(y2-y1)/(x2-x1)
+            //   移項之後：
+            //      x = (y-y1)*(x2-x1)/(y2-y1) + x1
+            final int x = (y - array[x1]) * (x2 - x1) / (array[x2] - array[x1]) + x1;
 
-            if (x > end || x < start) {
+            if (x > x2 || x < x1) { // 掃完了!
                 break;
             }
 
-            if (targetElement == array[x])
+            if (y == array[x])  // 掃到了!
                 return x;
-            else if (targetElement > array[x])
-                start = x + 1;
+            else if (y > array[x])
+                x1 = x + 1; // x1前進近似
             else
-                end = x - 1;
+                x2 = x - 1; // x2後退近似
         }
 
-        return -1;
+        return -1; // 沒掃到QQ
     }
 }
