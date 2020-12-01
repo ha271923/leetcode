d88f5061973c2a2ec6fb65c53b9130c73ef16582
ha271923 <ha271923@gmail.com>
2020-12-01 23:16:38 +0800

    update

--- app/src/main/java/com/cspirat/PalindromeNumber.java
+++ app/src/main/java/com/cspirat/PalindromeNumber.java
@@ -1,5 +1,7 @@
 package com.cspirat;
 
+import com.utils.Out;
+
 /**
  * Project Name : Leetcode
  * Package Name : leetcode
@@ -9,7 +11,10 @@ package com.cspirat;
  * Description : TODO
  */
 public class PalindromeNumber {
-
+    public static void main(String[] args) {
+        boolean ret = isPalindrome(121);
+        Out.i("ret="+ret);
+    }
     /**
      * 9. Palindrome Number
 
@@ -29,16 +34,45 @@ public class PalindromeNumber {
      *   Output: false
      * Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
      */
-    public boolean isPalindrome(int x) {
+    static public boolean isPalindrome(int x) {
         if (x < 0 ||
             x != 0 && x % 10 == 0) // last digital is 0, first is not possible 0.
             return false;
         int input = x;
         int rev = 0;
-        while (x > 0) {
+        while (x > 0) { // 把整個reverse後的值, 求出後, 數字比對
             rev = rev * 10 + x % 10; // KEY: value revers
             x /= 10;
         }
         return input == rev;
+    }
+
+
+
+    // Bug: 把數字當文字比對, 太慢
+    static public boolean isPalindrome_Hawk(int x) {
+        boolean res = true;
+        int len = 0;
+        int L=0,R=0;
+        if(x<0)
+            return false;
+        int temp =x;
+        while( temp!=0 ) { // calc len
+            temp = x /= 10;
+            len++;
+        }
+
+        if(len == 1) // only 1
+          return true;
+
+        while(len/2 >= 1) { // above 1
+            R = x % 10;
+            L = x / 10^len;
+            x = ( x - L*10^(len-1) - R )/ 10 ;
+            len--;
+            if( L != R )
+                return false;
+        }
+        return res;
     }
 }
