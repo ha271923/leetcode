e5f6cf47a66e6f0df1e91168549812918867a88f
ha271923 <ha271923@gmail.com>
2020-12-09 16:13:25 +0800

    update

--- app/src/main/java/com/cspirat/ContainerWithMostWater.java
+++ app/src/main/java/com/cspirat/ContainerWithMostWater.java
@@ -10,6 +10,10 @@ package com.cspirat;
  */
 public class ContainerWithMostWater {
 
+    public static void main(String[] args) {
+        int[] in = {1,8,6,2,5,4,8,3,7};
+        System.out.println("maxArea()="+maxArea(in));
+    }
     /**
      * time : O(n)
      * space : O(1)
@@ -21,21 +25,30 @@ public class ContainerWithMostWater {
     // Tips2: 長邊不動, 短邊動
     static public int maxArea(int[] height) {
         int res = 0;
-        int l = 0, r = height.length - 1; // l,r 為height[]的左右index
-        while (l < r) { // 左邊index不超過右邊index, 這樣可以形成一個container
-            int lowestSide = Math.min(height[l], height[r]); // 高 = 左邊y軸或是右邊y軸, 最低的那一個高度是y軸
-            int area =  lowestSide * (r - l); // 寬 = ( 右邊x軸 - 左邊x軸 ), 面積=x*y
+        int L = 0, R = height.length - 1; // L,R 為height[]的左右 X軸index
+        while (L < R) { // 掃描時 左邊index不得超過(或重疊)右邊index, 這樣可以形成一個container
+            int lowestSide = Math.min(height[L], height[R]); // 高 = 左邊y軸或是右邊y軸, 最低的那一個高度是y軸
+            int area =  lowestSide * (R - L); // 寬 = ( 右邊x軸 - 左邊x軸 ), 面積=x*y
             res = Math.max(res, area); // 破紀錄?
-            if (height[l] < height[r]) { // KEY: 長邊不動, 短邊動
-                l++;
+            if (height[L] < height[R]) { // KEY: 長邊不動, 短邊動
+                L++;
             } else
-                r--;
+                R--;
         }
         return res;
     }
 
-    public static void main(String[] args) {
-        int[] in = {1,8,6,2,5,4,8,3,7};
-        System.out.println("maxArea()="+maxArea(in));
+    static public int maxArea_hawk(int[] height) {
+        int L = 0;
+        int R = height.length-1;
+        int area = 0, res = 0;
+        while(L < R) {
+            area = height[L]*height[R];  // ERROR: a stupid fail 疏忽疏忽疏忽!
+            res = Math.max(res, area);
+            if(height[L] > height[R])
+                R--;
+            else
+                L++;
+        }
     }
 }
