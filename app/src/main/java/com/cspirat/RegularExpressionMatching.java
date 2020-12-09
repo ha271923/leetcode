716b2434b73e238beefde8708803a0b059c85b41
ha271923 <ha271923@gmail.com>
2020-12-09 15:36:20 +0800

    update

--- app/src/main/java/com/cspirat/RegularExpressionMatching.java
+++ app/src/main/java/com/cspirat/RegularExpressionMatching.java
@@ -2,6 +2,8 @@ package com.cspirat;
 
 // import com.hawk.leetcode.Basic.data.MyTree;
 
+import com.utils.Out;
+
 /**
  * Project Name : Leetcode
  * Package Name : leetcode
@@ -36,32 +38,35 @@ public class RegularExpressionMatching {
      isMatch("aab", "c*a*b") → true  注意!! 消去前面字
      isMatch("mississippi", "mis*is*p*.") → false
 
-     說明! 符號 * 的多種意義
-     "a*"  = "aa"           // 1. *: REPEAT: 代表與前一字元相同且重複次數不限
-     "a*b" = "aaab"         // 2. *: REPEAT: 代表與前一字元相同且重複次數不限
-     ".*"  = "aa"           // 3. *: REPEAT: 代表與前一字元相同
-     ".*"->"."+".*" = "ab"  // 4. *: ANY:    代表與前一字元相同, 若前一字元是 . , 代表可以是任意字元
-     "c*a*b" = "aab"        // 5. *: DEL:    代表DEL前一字元
+     {0~x} , {0~y}
+     說明! 符號 * 的多種意義 = NOTHING/REPEAT/ANY/DEL
+     "a*"  = "aa"              // 1. *: REPEAT:   重複前一字元(重複次數不限),所以結果會與前次dp[y][x-1]相同
+     "a*b" = "aaab"            // 2. *: REPEAT:   重複前一字元(重複次數不限),所以結果會與前次dp[y-1][x]相同, 直到 *符號與s的字串比對全部結束
+     ".*"  = "aa"              // 3. *: ANY+REPEAT:   重複前一字元(任意字元且重複次數不限),所以結果會與前次dp[y-1][x]相同, 直到 *符號與s的字串比對全部結束
+     ".*"->""+"."+".*" = "ab"  // 4. *: NOTHING+ANY+NOTHING, ANY+REPEAT:   重複前一字元(任意字元且重複次數不限),所以結果會與前次dp[y-1][x]相同, 直到 *符號與s的字串比對全部結束
+     "a*"  = "a"               // 5. *: NOTHING:  當作無作用的 *符號,所以結果會與前次dp[0~y][x-1]相同
+     "c*a*b" = "aab"           // 6. *: DEL:      代表DEL前一字
 
      HAWK: * 星星符號有多種狀態如上!!
 
-     boolean dp[i][j]的含义是s[0-i] 与 p[0-j]是否匹配。
+     boolean dp[y][x]的含义是s{0~y} 与 p{0~x}是否匹配。
 
-     c* = empty
+     c* = NOTHING
 
-     1，p.charAt(j) == s.charAt(i) : dp[i][j] = dp[i-1][j-1]
-     2，If p.charAt(j) == ‘.’ : dp[i][j] = dp[i-1][j-1];
-     3，If p.charAt(j) == ‘*’:
-         here are two sub conditions: Tips1: (j-1)代表取出前一字元
-         1，if p.charAt(j-1) != s.charAt(i) : dp[i][j] = dp[i][j-2] //in this case, a* only counts as empty
-         2，if p.charAt(j-1) == s.charAt(i) or p.charAt(j-1) == ‘.’:
-             dp[i][j] = dp[i][j-1] // in this case, a* counts as single a
-             dp[i][j] = dp[i-1][j] // in this case, a* counts as multiple a
-             dp[i][j] = dp[i][j-2] // in this case, a* counts as empty
+     1，p.charAt(x) == s.charAt(y) : dp[y][x] = dp[y-1][x-1]
+     2，If p.charAt(x) == ‘.’ : dp[y][x] = dp[y-1][x-1];
+     3，If p.charAt(x) == ‘*’:
+         here are two sub conditions: Tips1: (x-1)代表取出前一字元
+         1，if p.charAt(x-1) != s.charAt(y) :
+             dp[y][x] = dp[y][x-2] // in this case, a* only counts as empty
+         2，if p.charAt(x-1) == s.charAt(y) or p.charAt(x-1) == ‘.’:
+             dp[y][x] = dp[y][x-1] // in this case, a* counts as single a
+             dp[y][x] = dp[y-1][x] // in this case, a* counts as multiple a
+             dp[y][x] = dp[y][x-2] // in this case, a* counts as empty
 
      "aab", "c*aab"
 
-     i = 1 dp[0][2] = true
+     y = 1 dp[0][2] = true
 
      time : O(m * n)
      space : O(m * n)
@@ -74,56 +79,88 @@ public class RegularExpressionMatching {
      /*
      *         s="xaabyc", p="xa*b.*"  --> DP[s][p]
      *        重點在於 * 的規則!!!!
-     *                            x      a      *      b      .     *
+     *                    nil     x      a      *      b      .     *
      *        pattern= X=[0][x] [0][1] [0][2] [0][3] [0][4] [0][5] [0][6]  = "xa*b.*"
-     *text= Y=   [y][0] |  T   |  0   |  0   |  0   |  0   |  0   |  0    |
-     *        x  [1][0] |  0   |  T   |  0   |  T=-a|  0   |  0   |  0    |
-     *        a  [2][0] |  0   |  0   |  T   |  T=  |  0   |  0   |  0    |
-     *        a  [3][0] |  0   |  0   |  0   |  T=+a|  0   |  0   |  0    |
-     *        b  [4][0] |  0   |  0   |  0   |  0   |  T   |  0   |  0    |
-     *        y  [5][0] |  0   |  0   |  0   |  0   |  0   |  T=>y|  0    |
-     *        c  [6][0] |  0   |  0   |  0   |  0   |  0   |  0   | T=.*=c|
+     *text= Y=nil[y][0] |  T   |  0   |  0   |  0   |  0   |  0   |  0    |
+     *        x  [1][0] |  0   |  T   |  F   |  T=-a|  F   |  F   |  F    |
+     *        a  [2][0] |  0   |  F   |  T   |  T=  |  F   |  F   |  F    |
+     *        a  [3][0] |  0   |  F   |  F   |  T=+a|  F   |  F   |  F    |
+     *        b  [4][0] |  0   |  F   |  F   |  F   |  T   |  F   |  T    |
+     *        y  [5][0] |  0   |  F   |  F   |  F   |  F   |  T=>y|  T    |
+     *        c  [6][0] |  0   |  F   |  F   |  F   |  F   |  F   | T=.*=c|
      * */
 
     public static void main(String[] args) {
-        String s = "aab";
-        String p = "a*b";
+        String s = "xaabyc";
+        String p = "xa*b.*";
         System.out.println("isMatch()="+isMatch(s,p));
+
     }
 
     // Tips1: RegEx(Q10)裡的*跟DOS(Q44)裡的* , 意義不同
     // Tips2: 匹配真值表 dp[s.len+1]*[p.len+1], X與Y多一行列是因為 a* = null, a, aa, aaa... 有null的可能性
     static public boolean isMatch(String s, String p) {
-        if (s == null || p == null) return false;
-        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
-        // 1. True if pattern null to null
-        dp[0][0] = true; // 靠這個true傳遞下去dp[y][x]  https://youtu.be/l3hda49XcDE?t=303
+        Out.i("Test substring() API=  "+"0123456789".substring(0,3)); // API: substring() 會印出 012 , 不包含3
+        if (s == null || p == null)
+            return false;
+        boolean[][] dp = new boolean[s.length()+1][p.length()+1];
+        // 1. True if pattern=null to string=null
+        dp[0][0] = true; // 一開始什麼都沒有(注意! dp[y][0]與dp[0][x]全為false), 所以沒有當然匹配沒有 (s=NULL) == (p=NULL)靠這個true傳遞下去dp[y][x]  https://youtu.be/l3hda49XcDE?t=303
 
-        // 2. Deals with patterns  like a* a*b* a*b*c*
-        for (int x = 0; x < p.length(); x++) {
-            char c = p.charAt(x);
-            if (c == '*' && dp[0][x - 1]) { // 掃到* 並且前次相符
-                dp[0][x + 1] = true;
+
+        // KEY: charAt(x)/charAt(y) 比 dp[y+1][x+1] 差了1個位移
+        //      也就是說 charAt(y=3),charAt(x=5) 存在 dp[y=4][x=6]的空格
+
+        // 2. Deals with patterns , like a* a*b* a*b*c*
+        for (int x = 0; x < p.length(); x++) { // LOOP: DEL
+            char c = p.charAt(x); // 注意! 字元是x, 但是紀錄dp[][]時, 多一個位移+1
+            if (c == '*' && dp[0][x-1]) { // DEL: 掃到* 就算當DEL使用, 刪除一個字元,前次是true, 所以也會是一直true下去
+                Out.i("meet '*' ,dp[0][x-1] = true");
+                dp[0][x+1] = true;// 注意! 字元是x, 但是紀錄dp[][]時, 多一個位移+1
             }
         }
         // 3. Deals with other patterns
-        for (int y = 0; y < s.length(); y++) { // scan s String
-            for (int x = 0; x < p.length(); x++) { // scan p Pattern
-                if (p.charAt(x) == s.charAt(y)) { // single char matching
-                    dp[y + 1][x + 1] = dp[y][x];
+        // 當前pattern p.charAt(x)  , 當前string s.charAt(y)
+        // 當前pattern dp[?][x+1]   , 當前string dp[y+1][?]
+        for (int y = 0; y < s.length(); y++) { // LOOP1: 再增加一string字元供比對
+            for (int x = 0; x < p.length(); x++) { // LOOP2: 先掃pattern
+                Out.i("s="+s.substring(0,y+1)+"    p="+p.substring(0,x+1));
+                if (p.charAt(x) == s.charAt(y)) { // 標準單一字元比對
+                    Out.i("dp[y+1][x+1] = dp[y][x]");
+                    // 不能直接給true, 雖然當前過關, 但是dp是一個有延續的歷史紀錄, 而非真值表,
+                    // ex: (aa==aa) -> dp[1][1]=true, 但若為  (ba!=aa) -> dp[1][1]=false, 需參考歷史
+                    dp[y+1][x+1] = dp[y][x];
                 }
-                if (p.charAt(x) == '.') { // any char matching
-                    dp[y + 1][x + 1] = dp[y][x];
+                if (p.charAt(x) == '.') { // One char=ANY: 當遇到任意字元'.'
+                    Out.i("meet '.'  ,dp[y+1][x+1] = dp[y][x]");
+                    dp[y+1][x+1] = dp[y][x]; // 雖然'.'是任意字元, 但是dp[][]是歷史的累積, ex: (a.==ac) 但若為 (a.!=bc)
                 }
-                if (p.charAt(x) == '*') { //  '*' found
-                    if (p.charAt(x - 1) != s.charAt(y) && p.charAt(x - 1) != '.') { // NOT  aa==a*  or a*==.*
-                        dp[y + 1][x + 1] = dp[y + 1][x - 1];
-                    } else { // IS  aa==a*  or a*==.*
-                        dp[y + 1][x + 1] = (dp[y + 1][x] || dp[y][x + 1] || dp[y + 1][x - 1]);
+                /*
+     *        pattern= X=    [?][x-1]     [?][x]    [?][x+1]    [?][x+2]
+     *           [y-1][?] |            |           |           |         |
+     *string= Y= [y  ][?] |            |           |當前p前次s  |         |
+     *           [y+1][?] |當前s前前次p |當前s前次p  | <當前sp>   |         |
+     *           [y+2][?] |            |           |           |         |
+     *
+     * */
+                // NOTHING/REPEAT/ADD/DEL: 當遇到多功能字元'*' , 可能有多種處理狀況
+                if (p.charAt(x) == '*') { // *的各種處理結果, 將儲存在dp[0~y][*]的一整行
+                    if (p.charAt(x - 1) != '.' &&  // 如果前一個pattern字元不是萬用字元'.'
+                        p.charAt(x - 1) != s.charAt(y)) { // 並且也不是相同字母,  Ex:  ba* != aaa
+                        dp[y+1][x+1] =  dp[y+1][x-1];    // DEL: 參考舊的s字串的結果(p回退2符號), dp歷史紀錄要p往前前[x-1]查, [x-1]不怕OutOfBoundary麼?
+                        Out.i("meet '*'  ,dp[y+1][x+1] = dp[y+1][x-1]  MOT '.'");
+                    } else { // KEY: 只能死背!
+                        dp[y+1][x+1] = (dp[y+1][x  ] ||  // 遇到"*"符號時,當NOTHING用(回退1符號),  dp歷史紀錄要p往前[x  ]查(跟沒*一樣),   EX: a == (a*->a)
+                                        dp[y+1][x-1] ||  // 遇到".*" 符號時(回退2符號), dp歷史紀錄要p往前前[x-1]查, EX: bcc != a.* , acc == a.*
+                                        dp[y  ][x+1] );  // 遇到"*"符號時,當REPEAT用(回退1字元),   dp歷史紀錄要s往前[y  ]查,   EX: aab->aa == a* ,  aa* == a
+                        Out.i("meet '*'  ,dp[y+1][x+1] = dp[y+1][x] || dp[y][x+1] || dp[y+1][x-1]");
                     }
                 }
+                Out.i("dp["+(y+1)+"]["+(x+1)+"] = " + dp[y+1][x+1]);
+                Out.i("----------------------------------------------------");
             }
         }
+        Out.print2DArray(dp);
         return dp[s.length()][p.length()];
     }
 
@@ -152,48 +189,49 @@ public class RegularExpressionMatching {
     }
     static Result[][] memo;
     static public boolean isMatchDP(String text, String pattern) {
-        memo = new Result[text.length() + 1][pattern.length() + 1];
+        memo = new Result[text.length()+1][pattern.length()+1];
         return dp(0, 0, text, pattern);
     }
-    static public boolean dp(int i, int j, String text, String pattern) {
-        if (memo[i][j] != null) {
-            return memo[i][j] == Result.TRUE;
+    static public boolean dp(int y, int x, String text, String pattern) {
+        if (memo[y][x] != null) {
+            return memo[y][x] == Result.TRUE;
         }
         boolean ans;
-        if (j == pattern.length()){
-            ans = i == text.length();
+        if (x == pattern.length()){
+            ans = y == text.length();
         } else{
-            boolean first_match = (i < text.length() &&
-                    (pattern.charAt(j) == text.charAt(i) ||
-                            pattern.charAt(j) == '.'));
+            boolean first_match = (y < text.length() &&
+                    (pattern.charAt(x) == text.charAt(y) ||
+                            pattern.charAt(x) == '.'));
 
-            if (j + 1 < pattern.length() && pattern.charAt(j+1) == '*'){
-                ans = (dp(i, j+2, text, pattern) ||
-                        first_match && dp(i+1, j, text, pattern));
+            if (x+1 < pattern.length() && pattern.charAt(x+1) == '*'){
+                ans = (dp(y, x+2, text, pattern) ||
+                        first_match && dp(y+1, x, text, pattern));
             } else {
-                ans = first_match && dp(i+1, j+1, text, pattern);
+                ans = first_match && dp(y+1, x+1, text, pattern);
             }
         }
-        memo[i][j] = ans ? Result.TRUE : Result.FALSE;
+        memo[y][x] = ans ? Result.TRUE : Result.FALSE;
         return ans;
     }
 
     static public boolean isMatchDP2(String text, String pattern) {
-        boolean[][] dp = new boolean[text.length() + 1][pattern.length() + 1];
+        boolean[][] dp = new boolean[text.length()+1][pattern.length()+1];
         dp[text.length()][pattern.length()] = true;
 
-        for (int i = text.length(); i >= 0; i--){
-            for (int j = pattern.length() - 1; j >= 0; j--){
-                boolean first_match = (i < text.length() &&
-                        (pattern.charAt(j) == text.charAt(i) ||
-                                pattern.charAt(j) == '.'));
-                if (j + 1 < pattern.length() && pattern.charAt(j+1) == '*'){
-                    dp[i][j] = dp[i][j+2] || first_match && dp[i+1][j];
+        for (int y = text.length(); y >= 0; y--){
+            for (int x = pattern.length() - 1; x >= 0; x--){
+                boolean first_match = (y < text.length() &&
+                        (pattern.charAt(x) == text.charAt(y) ||
+                                pattern.charAt(x) == '.'));
+                if (x+1 < pattern.length() && pattern.charAt(x+1) == '*'){
+                    dp[y][x] = dp[y][x+2] || first_match && dp[y+1][x];
                 } else {
-                    dp[i][j] = first_match && dp[i+1][j+1];
+                    dp[y][x] = first_match && dp[y+1][x+1];
                 }
             }
         }
         return dp[0][0];
     }
+
 }
