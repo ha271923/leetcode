26dee82ef08a97faaaed86641c668fde45ca7347
ha271923 <ha271923@gmail.com>
2020-12-10 17:02:58 +0800

    update

--- app/src/main/java/com/cspirat/IntegertoRoman.java
+++ app/src/main/java/com/cspirat/IntegertoRoman.java
@@ -1,5 +1,7 @@
 package com.cspirat;
 
+import java.util.HashMap;
+
 /**
  * Project Name : Leetcode
  * Package Name : leetcode
@@ -26,11 +28,12 @@ public class IntegertoRoman {
      */
     // 觀察符號系統的拆解方式
     static public String intToRoman(int num) {
-        // Tips: 羅馬數字 高位數的在左邊, 低位數在右邊, IV=4, VI=6=V+I可由5+1組合
+        // Tips: 羅馬數字 高位數的在左邊, 低位數在右邊, IV=4, VI=6=V+I可由5+1組合, KEY: 組合數字 4,9 無法用加法算出, 需定義符號直接查表
+        //  1    2     3     4    5    6     7     8      9    10
+        //  I    II   III   IV    V    VI    VII   VIII   IX    X   數字1 4 5 9 10, 需要直接查表, 無法計算
         int[] values =  {1000, 900,500,400 ,100,  90, 50,  40, 10,   9,  5,   4, 1 };  // KEY: 最大值在index=0, 設定位階, 從上(大)掃到下(小)
-        //                *         *        *        *        *        *        *     // KEY: 題目沒有的IV, VI , 要如何定義位階表, 新增"IV"=4, 忽略"VI"=6
+        // 編碼系統的重要性 *         *        *        *        *        *        *     // KEY: 題目沒有的IV, VI , 要如何定義位階表, 新增"IV"=4, 忽略"VI"=6
         String[] strs = {"M" ,"CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};  // KEY: 4=IV , 5=V 數字大小與字母數量沒有正相關
-
 
         StringBuilder sb = new StringBuilder();
 
